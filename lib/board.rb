604b641444865fd53a40b4d04237fb4bf5e786a6
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-05-22 03:23:39 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -7,13 +7,12 @@ def cells
 end
 
 def reset!
-
 @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-@cells << cells
 end
 
 def initialize
-  self.reset!
+  @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 end
 
 end
