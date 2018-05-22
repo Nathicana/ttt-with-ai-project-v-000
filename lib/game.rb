5e59a72aa7a2b565ae248bda054e7b56820954ac
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-05-22 04:30:33 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,17 @@
 class Game
 
-def initialize(player1 = "X", player2 = "O", board)
-  @player1 = "X"
-  @player2 = "O"
-  @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-end
+  class Game
+    
+    attr_accessor :board, :player_1, :player_2
 
+    WIN_COMBINATIONS = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [6,4,2]
+    ]
 end
