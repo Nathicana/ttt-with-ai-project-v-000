305da51ccb9b4752ce417c1f88aca43b2dd67731
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-05-22 03:01:19 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,9 @@
 class Game
 
 def initialize(player1 = "X", player2 = "O", board)
-  @player1 = player1 
-  @player2 = player2 
-  @board = board
+  @player1 = "X"
+  @player2 = "O"
+  @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 end
 
 end
